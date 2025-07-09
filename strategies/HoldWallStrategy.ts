fc034fa2ea2e85c60f54107e9d05c1d39b657865
RobinSpyra <ugfun@student.kit.edu>
2025-07-09 13:49:20 +0200

    hold left and right strategy (maybe working)

---
+++ strategies/HoldWallStrategy.ts
@@ -0,0 +1,24 @@
+import {Direction, Environment, Strategy} from "../util";
+
+export abstract class HoldWallStrategy extends Strategy {
+    previousDirection : Direction = "U"
+
+    getDirection(environment: Environment): Direction {
+        if (this.checkDirection(environment, this.getNecessaryDirection())) {
+            this.previousDirection = this.getNecessaryDirection()
+            return this.previousDirection
+        } else {
+            this.previousDirection = this.getNecessaryDirection()
+            return this.getDirection(environment)
+        }
+    }
+
+    checkDirection(environment: Environment, direction: Direction) : boolean {
+        return environment.get(direction) != 'X'
+    }
+
+    abstract getNecessaryDirection() : Direction
+
+
+
+}
