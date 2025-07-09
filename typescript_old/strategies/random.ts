c26cc86e2648a6e195afc368032ab53d6ec18a1a
ubxft <ubxft@student.kit.edu>
2025-07-09 23:56:09 +0200

    DELETE REDUNDANT FILES!!!

--- typescript_old/strategies/random.ts
+++
@@ -1,8 +0,0 @@
-import { Direction, Environment, Strategy } from "../util"
-
-export class RandomStrategy extends Strategy {
-    getDirection(environment: Environment): Direction {
-        const index: number = Math.floor(4 * Math.random())
-        return environment.keys[index]
-    }
-}
