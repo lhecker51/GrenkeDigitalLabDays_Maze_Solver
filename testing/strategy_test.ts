11e30d295be40e6d9484961c281e7b5d0d719e16
emilia <emiliaschuetz1702@gmail.com>
2025-07-09 15:30:52 +0200

    add wrapper

--- testing/strategy_test.ts
+++ testing/strategy_test.ts
@@ -1,11 +1,11 @@
-import { generator } from "../generator/generator.js"
+import { generatorWrapper } from "../generator/wrapper.js"
 import { Path, Labyrinth, Strategy } from "../util"
 import { RandomStrategy } from "../strategies/random"
 import { solver } from "../solver/solver"
 
 class test {
     static test() {
-        const labyrinth: Labyrinth = <Labyrinth>generator.generateDfs(11)
+        const labyrinth: Labyrinth = <Labyrinth>generatorWrapper.generateDFSLabyrinth(11)
         const randomStrategy: Strategy = new RandomStrategy()
         const path: Path = solver.solve(randomStrategy, labyrinth)
         console.log(path)
