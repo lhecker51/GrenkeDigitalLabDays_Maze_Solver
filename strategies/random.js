d1ba713d84e0559bd102bdb296a46b51d8ae1184
ubxft <ubxft@student.kit.edu>
2025-07-10 10:02:49 +0200

    add random strategy

---
+++ strategies/random.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import {Strategy} from "../backend/utils.js";
+
+export class RandomStrategy extends Strategy {
+    calculateDirection(environment) {
+        let direction = this.getRandomDirection()
+        while (!this.checkDirection(environment, direction)) {
+            direction = this.getRandomDirection()
+        }
+        return direction
+    }
+
+    getRandomDirection() {
+        const index = Math.floor(4 * Math.random())
+        return ["U", "D", "L", "R"][index]
+    }
+}
